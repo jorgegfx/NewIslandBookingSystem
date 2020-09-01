62684b1a427e312921aa2a247a4945f2be8244fd
Jorge Gonzalez <jorgegfx@gmail.com>
2020-09-01 17:45:05 -0400

    Gateway Microservice + Kafka

---
+++ reservation-service/src/main/java/com/newisland/reservation/service/ReservationCommandListener.java
@@ -0,0 +1,43 @@
+package com.newisland.reservation.service;
+
+import com.google.protobuf.Timestamp;
+import com.newisland.common.messages.command.CreateReservationCommandOuterClass;
+import com.newisland.reservation.model.entity.Reservation;
+import com.newisland.reservation.model.entity.ReservationStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class ReservationCommandListener {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    private Instant convertToInstant(Timestamp timestamp){
+        return Instant.ofEpochMilli(timestamp.getSeconds());
+    }
+
+    @KafkaListener(topics = "reservation")
+    public void consume(ConsumerRecord<String, byte[]> message) {
+        log.info(String.format("#### -> Consumed message -> %s", message));
+        try {
+            CreateReservationCommandOuterClass.CreateReservationCommand cmd =
+                    CreateReservationCommandOuterClass.CreateReservationCommand.parseFrom(message.value());
+            Reservation reservation = Reservation.builder().
+                    campsiteId(UUID.fromString(cmd.getCampsiteId())).
+                    arrivalDate(convertToInstant(cmd.getArrivalDate())).
+                    departureDate(convertToInstant(cmd.getDepartureDate())).
+                    build();
+            reservationService.save(reservation);
+        }catch (Exception ex){
+            log.error("Error reading message ...",ex);
+        }
+    }
+}
